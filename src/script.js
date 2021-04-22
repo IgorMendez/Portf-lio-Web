adde27301a46038cf192d1aee23b67c8ddf1027c
IgorMendez <igor.emp@hotmail.com>
2021-04-22 04:43:59 -0300

    Estruturação CSS e criação funções JS

--- src/script.js
+++ src/script.js
@@ -1,17 +1,19 @@
 // Variables
 const body = document.querySelector('body')
-const mainContainer = document.querySelector('main')
+const main = document.querySelector('main')
 const menu = document.getElementById('menu')
+const mainContainer = document.getElementById('main-container')
+const getButton = document.getElementsByClassName('button')
 const buttonArray = [
   'Sobre mim', 
   'Habilidades', 
   'Formação', 
   'Experiências',
   'Contatos',
-'Informações Gerais'
+  'Informações Gerais'
 ]
 
-console.log(menu)
+// console.log()
 // mainContainer.children.namedItem('header-section')
 
 function buttomMenuCreate(e) {
@@ -19,10 +21,25 @@ function buttomMenuCreate(e) {
     const createbutton = document.createElement('button')
     menu.appendChild(createbutton)
     createbutton.innerText = buttonArray[index]
-    createbutton.classList.add('button')
+    createbutton.classList.add('button', 'btn-dark')
+    createbutton.id = buttonArray[index]
+  }
+}
+
+function showContainer() {
+  let oi = Array.from(mainContainer.children)
+  for(let index = 0; index < buttonArray.length; index += 1) {
+    oi[index].classList.add('container-hidden')
+  }
+}
+
+function clickButton(target.event) {
+  if(target.event === 'click') {
+    clickButton()
   }
 }
 
 window.onload = function() {
   buttomMenuCreate(buttonArray)
+  showContainer()
 }
